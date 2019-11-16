230de676cc50fc742378d250da42a676968938a3
zeronedev <ok7375@gmail.com>
2019-11-17 07:01:49 +0900

    2.3 map Recap done

--- src/App.js
+++ src/App.js
@@ -1,47 +1,51 @@
 import React from "react";
 
-function Food({ name, picture }) {
-  return (
-    <div>
-      <h2>I like {name}</h2>
-      <img src={picture} alt=""></img>
-    </div>
-  );
-}
-
 const foodILike = [
   {
+    id: 1,
     name: "Kimchi",
     image:
       "https://www.maangchi.com/wp-content/uploads/2013/03/baechukimchi-150x150.jpg"
   },
   {
+    id: 2,
     name: "Ramen",
     image:
       "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fb/Ramyun_cooking_image.png/220px-Ramyun_cooking_image.png"
   },
   {
+    id: 3,
     name: "Samgiopsal",
     image:
       "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b4/Samgyeopsal_%28pork_belly%29.jpg/220px-Samgyeopsal_%28pork_belly%29.jpg"
   },
   {
+    id: 4,
     name: "Chukumi",
     image: "https://blogthatcake.files.wordpress.com/2013/08/113.jpg"
   },
   {
+    id: 5,
     name: "Bibimbap",
     image:
       "https://upload.wikimedia.org/wikipedia/commons/thumb/4/44/Dolsot-bibimbap.jpg/220px-Dolsot-bibimbap.jpg"
   }
 ];
 
+function Food({ name, picture }) {
+  return (
+    <div>
+      <h2>I like {name}</h2>
+      <img src={picture} alt={name} />
+    </div>
+  );
+}
+
 function App() {
   return (
     <div>
-      <h1>Hello!!!</h1>
       {foodILike.map(dish => (
-        <Food name={dish.name} picture={dish.image} />
+        <Food key={dish.id} name={dish.name} picture={dish.image} />
       ))}
     </div>
   );
